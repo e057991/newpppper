8877ce94aef9d7c9d3ba35c6203c61ba465aea99
e057991 <e057991@gmail.com>
2020-05-11 22:47:10 +0800

    Add files via upload

--- bellllapaper/js/backtotop.js
+++ bellllapaper/js/backtotop.js
@@ -1,5 +1,6 @@
 //Get the button
 var mybutton = document.getElementById("myBtn");
+var mybutton1 = document.getElementById("myPhone");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -7,8 +8,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    mybutton1.style.display = "block";
   } else {
     mybutton.style.display = "none";
+    mybutton1.style.display = "none";
   }
 }
 
